132c0ed4e05b7a687f98607e9789f26682fe4795
Valdeijr <valdei.ferreira.jr@gmail.com>
2024-10-25 20:46:01 -0300

    Desfumando as fontes

--- Local/js/registration.js
+++ Local/js/registration.js
@@ -1,12 +1,4 @@
 // Created and tested by: Russo - Valdeijr
-
-// animate
-$('#password').focusIn(function(){
-    $('form').addClass('up')
-  });
-  $('#password').focusout(function(){
-    $('form').removeClass('up')
-});
   
 // validation
 $('.btn').click(function(){
